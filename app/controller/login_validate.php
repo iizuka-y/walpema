2b74fef0ac24286018ecfd0474b363c3d4bdf7fc
iizuka <mesicharida202@gmail.com>
2020-12-08 22:04:36 +0900

    ログイン時にセッションに保存したカートの中身をデータベースに移すようにした

--- app/controller/login_validate.php
+++ app/controller/login_validate.php
@@ -1,6 +1,7 @@
 <?php
 require_once(dirname(__FILE__).'/../fn_components/session_cookie.php');
 require_once(dirname(__FILE__).'/../class/User.php');
+require_once(dirname(__FILE__).'/../fn_components/cart_processing.php');
 
 
 if(!isset($_POST['email']) || !isset($_POST['password'])){
@@ -32,6 +33,9 @@ if(!empty($errorMsg)){
     $user = User::find($params);
     // print_r($user);
     if($user){
+        // sessionのカートに商品が入っていた場合はDBのカートテーブルに移す。
+        cartChangeToDB();
+
         fnc_setData("session", "login_userId", $user->id);
         header("Location: ../../view/index.php");
     }else{
@@ -40,6 +44,23 @@ if(!empty($errorMsg)){
         header("Location: ../../view/login.php");
     }
 
+}
+
+
+function cartChangeToDB(){
+    global $user;
+    $cart_items = get_cartItem();
+    foreach($cart_items as $cart_item){
+        $params = [
+            'user_id' => $user->id,
+            'item_id' => $cart_item->id
+        ];
+        if(!Purchase_history::find($params)){
+            // すでに買ったことがある商品はカートに入れない
+            Cart::create($params);
+        }
+        
+    }
 }
 
 
